de843ec9baa7ca9c3f0dacdc31c1f3af50860cb5
marat223 <pcgeniusweb.mm@gmail.com>
2017-12-07 21:44:12 +0300

    project

--- src/by/it/mustaphin/project/java/controller/FrontController.java
+++ src/by/it/mustaphin/project/java/controller/FrontController.java
@@ -1,30 +1,33 @@
 package by.it.mustaphin.project.java.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class FrontController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+	ICommand cmd = new ActionFactory().getCommand(req);
+	ICommand nextAction = cmd.execute(req);
+    	if (nextAction == null) {
+	    req.getRequestDispatcher(cmd.getJsp()).forward(req, resp);
+	} else {
+	    resp.sendRedirect("do?command=" + nextAction);
+	}
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+	ICommand cmd = new ActionFactory().getCommand(req);
+	ICommand nextAction = cmd.execute(req);
+	if (nextAction == null) {
+	    req.getRequestDispatcher(cmd.getJsp()).forward(req, resp);
+	} else {
+	    resp.sendRedirect("do?command=" + nextAction);
+	}
     }
 
-    private void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        ICommand cmd = new ActionFactory().getCommand(req);
-        ICommand nextAction = cmd.execute(req);
-        if (null == nextAction) {
-            req.getRequestDispatcher(cmd.getJsp()).include(req, res);
-        } else {
-            res.sendRedirect(nextAction.getJsp());
-        }
-    }
 }
--- src/by/it/mustaphin/project/webapp/signup.jsp
+++ src/by/it/mustaphin/project/webapp/signup.jsp
@@ -1,6 +1,20 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
 <%@ include file="include/begin-html.jsp" %>
 
-<p>Cmd SIGN-UP: ${message}</p>
+<form action="do?command=SignUp" method="post">
+
+    <input id="login" name="login" type="text"
+	   placeholder="" class="form-control input-md" required=""
+	   value="TestLogin">
+    <input id="password" name="password" type="password" placeholder="" class="form-control input-md"
+	   required=""
+	   value="TestPassword">
+    <input id="email" name="email" type="text" placeholder="email@domain" class="form-control input-md"
+	   required=""
+	   value="email@email.com">
+    <button name="submit">Submit</button>
+</form>
+<!-- Text input-->
+<p>message=${message}</p>
 
 <%@ include file="include/end-html.jsp" %>
